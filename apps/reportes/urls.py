e17021d4e2185a116365e0ddc18c690044caffdf
tecnologiamegabahia <79868992+tecnologiamegabahia@users.noreply.github.com>
2021-03-29 15:33:56 -0500

    init

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from django.urls import path
 from .views import reporte_cumpleanios_children, index, conteo_cliente, conteo_contribuyentes_naturales, \
-    conteo_contribuyentes_juridicos, conteo_empleados, form_view_combo, reporte_negocio
+    conteo_contribuyentes_juridicos, conteo_empleados, form_view_combo, reporte_negocio, reporte_cumpleanios_children_ci
 
 urlpatterns = [
     url(r'^index$', login_required(index), name='index'),
@@ -13,6 +13,7 @@ urlpatterns = [
         name='conteo_contribuyentes_juridicos'),
     url(r'^conteo_empleados/$', login_required(conteo_empleados), name='conteo_empleados'),
     path('combo/', form_view_combo.as_view(), name='combo'),
-    path('reporte_negocio/', login_required(reporte_negocio), name='reporte_negocio')
+    path('reporte_negocio/', login_required(reporte_negocio), name='reporte_negocio'),
+    url(r'^dowload_report_ci/$', login_required(reporte_cumpleanios_children_ci), name='dowload_report_ci'),
 
 ]
--- apps/reportes/views.py
+++ apps/reportes/views.py
@@ -183,6 +183,10 @@ def index(request):
     return render(request, 'reportes/reporte_cumpleanios_children.html')
 
 
+def index_ci(request):
+    return render(request, 'reportes/reporte_cumpleanios_children_ci.html')
+
+
 def conteo_cliente(request):
     try:
         cursor = connection.cursor()
@@ -254,5 +258,308 @@ class form_view_combo(TemplateView):
 
 
 def reporte_negocio(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="reporte_negocios.xls"'
+    cursor = connection.cursor()
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('reporte_cumpleanios_children')
 
-    return True
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = [
+        'RAZON_SOCIAL',
+        'NOMBRE_FANTASIA_COMERCIAL',
+        'FECHA_INSCRIPCION_RUC',
+        'FECHA_INICIO_ACTIVIDADES',
+        'OBLIGADO',
+        'FECHA_NACIMIENTO',
+        'FECHA_REINICIO_ACTIVIDADES',
+        'TELEFONO',
+        'CORREO_ELECTRONICO',
+        'ACTIVIDAD_ECONOMICA_PRINCIPAL',
+        'ESTADO_PERSONA_NATURAL',
+        'DESCRIPCION_LUGAR_UBICACION',
+        'CODIGO_ECONOMICA_PRINCIPAL',
+        'ACTIVIDAD_ECONOMICA_PRINCIPAL',
+        'CODIGO_TIPO_CONTACTO_1',
+        'CELULAR_1',
+        'CORREO_1',
+        'CODIGO_TIPO_CONTACTO_2',
+        'CELULAR_2',
+        'CORREO_2',
+        'CODIGO_TIPO_CONTACTO_3',
+        'CELULAR_3',
+        'CORREO_3',
+        'CODIGO_TIPO_CONTACTO_4',
+        'CELULAR_4',
+        'CORREO_4',
+        'CODIGO_TIPO_CONTACTO_5',
+        'CELULAR_5',
+        'CORREO_5',
+        'CODIGO_TIPO_CONTACTO_6',
+        'CELULAR_6',
+        'CORREO_6',
+        'CODIGO_TIPO_CONTACTO_7',
+        'CELULAR_7',
+        'CORREO_7',
+        'CODIGO_TIPO_CONTACTO_8',
+        'CELULAR_8',
+        'CORREO_8',
+        'CODIGO_TIPO_CONTACTO_9',
+        'CELULAR_9',
+        'CORREO_9',
+        'CODIGO_TIPO_CONTACTO_10',
+        'CELULAR_10',
+        'CORREO_10'
+    ]
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+    form = request.POST or None
+    print(form)
+
+    economica_p = form['nivel7']
+    ciudad_p = request.POST.get('txtvariable1', False)
+    contribuyente_p = form['Contribuyente']
+    estado_p = form['Estado']
+    fechai_p = form['date_time']
+    fechaf_p = form['date_time']
+    if fechai_p == '':
+        fi = datetime.strptime('1900-01-01', "%Y-%m-%d").strftime('%d/%m/%Y')
+        ff = datetime.strptime('2030-01-01', "%Y-%m-%d").strftime('%d/%m/%Y')
+    else:
+        fi = datetime.strptime(fechai_p, "%Y-%m-%d").strftime('%d/%m/%Y')
+        ff = datetime.strptime(fechaf_p, "%Y-%m-%d").strftime('%d/%m/%Y')
+
+    cursor.execute(
+        "CALL reporte_negocios('" + economica_p + "','" + ciudad_p + "','" + contribuyente_p + "','" + estado_p + "','"
+        + str(fi) + "','" + str(ff) + "')")
+
+    rows = cursor.fetchall()
+    list = []
+    for row in rows:
+        try:
+            rowss = [(cipher.decrypt(row[0])),
+                     (row[1]),
+                     (cipher.decrypt(row[2])),
+                     (cipher.decrypt(row[3])),
+                     (row[4]),
+                     (cipher.decrypt(row[5])),
+                     (row[6]),
+                     (cipher.decrypt(row[7])),
+                     (cipher.decrypt(row[8])),
+                     (row[9]),
+                     (row[10]),
+                     (row[11]),
+                     (row[12]),
+                     (row[13]),
+                     (row[14]),
+                     (row[15]),
+                     (row[16]),
+                     (row[17]),
+                     (row[18]),
+                     (row[19]),
+                     (row[20]),
+                     (row[21]),
+                     (row[22]),
+                     (row[23]),
+                     (row[24]),
+                     (row[25]),
+                     (row[26]),
+                     (row[27]),
+                     (row[28]),
+                     (row[29]),
+                     (row[30]),
+                     (row[31]),
+                     (row[32]),
+                     (row[33]),
+                     (row[34]),
+                     (row[35]),
+                     (row[36]),
+                     (row[37]),
+                     (row[38]),
+                     (row[39]),
+                     (row[40]),
+                     (row[41]),
+                     (row[42]),
+                     (row[43])]
+        except IndexError as e:
+            break
+        list.append(rowss)
+    print(list)
+    for row in list:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
+
+
+def reporte_cumpleanios_children_ci(request):
+    form = request.POST or None
+    ci_p = form['ci_p']
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="reporte_cumpleanios_children_ci' + ci_p + '.xls"'
+    cursor = connection.cursor()
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('reporte_cumpleanios_children_ci')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = [
+        'NOMBRES_Y_APELLIDOS',
+        'FECHA_NACIMIENTO',
+        'AÑOS_CUMPLIDOS',
+        'NOMBRE_CANTON',
+        'DESCRIPCION_SEXO',
+        'NOMBRES_DEL_PADRE',
+        'FECHA_NACIMIENTO_PADRE',
+        'AÑOS_CUMPLIDOS_PADRE',
+        'LUGAR_NACIMIENTO_PADRE',
+        'NIVEL_ESTUDIOS_PADRE',
+        'PROFESION_PADRE',
+        'SEXO_PADRE',
+        'CELULAR_PADRE_1',
+        'CELULAR_PADRE_2',
+        'CELULAR_PADRE_3',
+        'CELULAR_PADRE_4',
+        'CELULAR_PADRE_5',
+        'CELULAR_PADRE_6',
+        'CELULAR_PADRE_7',
+        'CELULAR_PADRE_8',
+        'CELULAR_PADRE_9',
+        'CELULAR_PADRE_10',
+        'CORREO_PADRE_1',
+        'CORREO_PADRE_2',
+        'CORREO_PADRE_3',
+        'CORREO_PADRE_4',
+        'CORREO_PADRE_5',
+        'CORREO_PADRE_6',
+        'CORREO_PADRE_7',
+        'CORREO_PADRE_8',
+        'CORREO_PADRE_9',
+        'CORREO_PADRE_10',
+        'NOMBRES_DE_LA_MADRE',
+        'FECHA_NACIMIENTO_MADRE',
+        'AÑOS_CUMPLIDOS_MADRE',
+        'NIVEL_ESTUDIOS_MADRE',
+        'PROFESION_MADRE',
+        'SEXO_MADRE',
+        'CELULAR_MADRE_1',
+        'CELULAR_MADRE_2',
+        'CELULAR_MADRE_3',
+        'CELULAR_MADRE_4',
+        'CELULAR_MADRE_5',
+        'CELULAR_MADRE_6',
+        'CELULAR_MADRE_7',
+        'CELULAR_MADRE_8',
+        'CELULAR_MADRE_9',
+        'CELULAR_MADRE_10',
+        'CORREO_MADRE_1',
+        'CORREO_MADRE_2',
+        'CORREO_MADRE_3',
+        'CORREO_MADRE_4',
+        'CORREO_MADRE_5',
+        'CORREO_MADRE_6',
+        'CORREO_MADRE_7',
+        'CORREO_MADRE_8',
+        'CORREO_MADRE_9',
+        'CORREO_MADRE_10'
+    ]
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    ci_p = form['ci_p']
+
+    cursor.execute(
+        "CALL reporte_cumpleanios_children_ci('" + ci_p + "')")
+
+    rows = cursor.fetchall()
+    list = []
+    for row in rows:
+        try:
+            rowss = [(cipher.decrypt(row[0])),
+                     (row[1]),
+                     (row[2]),
+                     (row[3]),
+                     (row[4]),
+                     (cipher.decrypt(row[5])),
+                     (row[6]),
+                     (row[7]),
+                     (row[8]),
+                     (row[9]),
+                     (row[10]),
+                     (row[11]),
+                     (row[12]),
+                     (row[13]),
+                     (row[14]),
+                     (row[15]),
+                     (row[16]),
+                     (row[17]),
+                     (row[18]),
+                     (row[19]),
+                     (row[20]),
+                     (row[21]),
+                     (row[22]),
+                     (row[23]),
+                     (row[24]),
+                     (row[25]),
+                     (row[26]),
+                     (row[27]),
+                     (row[28]),
+                     (row[29]),
+                     (row[30]),
+                     (row[31]),
+                     (cipher.decrypt(row[32])),
+                     (row[33]),
+                     (row[34]),
+                     (row[35]),
+                     (row[36]),
+                     (row[37]),
+                     (row[38]),
+                     (row[39]),
+                     (row[40]),
+                     (row[41]),
+                     (row[42]),
+                     (row[43]),
+                     (row[44]),
+                     (row[45]),
+                     (row[46]),
+                     (row[47]),
+                     (row[48]),
+                     (row[49]),
+                     (row[50]),
+                     (row[51]),
+                     (row[52]),
+                     (row[53]),
+                     (row[54]),
+                     (row[55]),
+                     (row[56]),
+                     (row[57])]
+        except IndexError as e:
+            break
+        list.append(rowss)
+    print(list)
+    for row in list:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
